bf19b6911b3e0a774316d34008ad704bb4e0ceed
4kangjc <1158035520@qq.com>
2023-03-26 12:54:27 +0800

    Initial commit

---
+++ raft/raft.h
@@ -0,0 +1,9 @@
+#ifndef FLARE_RAFT_RAFT_H_
+#define FLARE_RAFT_RAFT_H_
+
+#include "flare/fiber/timer.h"
+#include "flare/rpc/server.h"
+
+namespace flare::raft {}  // namespace flare::raft
+
+#endif  // FLARE_RAFT_RAFT_H_
